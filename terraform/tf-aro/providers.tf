4df63633fdf2e4cec56d4af6a535dcf53b58364c
Paul Czarkowski <username.taken@gmail.com>
2024-08-14 17:27:35 -0400

    add ARO
    Signed-off-by: Paul Czarkowski <username.taken@gmail.com>

---
+++ terraform/tf-aro/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~>2.53"
+    }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.114"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
